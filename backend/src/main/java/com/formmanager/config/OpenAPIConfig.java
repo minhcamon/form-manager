9b45666afb51fc74924fbc93cb025dd340ade0f5
Minh <nhatminhyt06@gmail.com>
2026-07-01 21:00:23 +0700

    chore: add openAPI config and env.properties

---
+++ backend/src/main/java/com/formmanager/config/OpenAPIConfig.java
@@ -0,0 +1,34 @@
+package com.formmanager.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Form-Manager Backend API")
+                        .version("1.0.0")
+                        .description("Tài liệu tích hợp và kiểm thử toàn bộ hệ thống API của dự án Form-Manager.")
+                )
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer")
+                .description("Nhập token JWT của bạn để xác thực API (Không cần gõ chữ 'Bearer ', chỉ cần paste token).");
+    }
+}
